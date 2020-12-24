c853b2a8ca16ff5408d4880323dfac3c02fb284f
reynosojoaquin@hotmail.com <reynosojoaquin@hotmail.com>
2020-12-24 12:35:59 -0400

    inicio del repositorio

---
+++ toDoapp-Spa/src/layouts/MainLayout.vue
@@ -0,0 +1,73 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+
+        <q-toolbar-title>
+          To DO List
+        </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-grey-1"
+    >
+      <q-list>
+        <q-item-label
+          header
+          class="text-grey-8"
+        >
+         Main Menu
+        </q-item-label>
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script lang="ts">
+import EssentialLink from 'components/EssentialLink.vue'
+
+const linksData = [
+  {
+    title: 'TODO',
+    caption: 'Lista de tareas',
+    icon: 'fact_check',
+    link: 'https://quasar.dev'
+  }
+]
+
+import { defineComponent, ref } from '@vue/composition-api'
+
+export default defineComponent({
+  name: 'MainLayout',
+  components: { EssentialLink },
+  setup () {
+    const leftDrawerOpen = ref(false)
+    const essentialLinks = ref(linksData)
+
+    return { leftDrawerOpen, essentialLinks }
+  }
+})
+</script>
---
+++ toDoapp-Spa/src/pages/Index.vue
@@ -0,0 +1,136 @@
+<template>
+  <q-page padding>
+    <div class="text-h2 text-grey-8 ">
+      TO DO LIST
+      <span>
+        <q-img src="../assets/todolist.png" style="height:100px;width:100px" />
+      </span>
+    </div>
+
+    <div class="row q-mt-sm">
+      <div class="column col-12  items-center justify-evenly ">
+        <q-card class="card-header ">
+          <q-card-section>
+            <div class="text-h6">Tareas</div>
+          </q-card-section>
+          <q-separator></q-separator>
+          <q-card-section
+           
+            id="task-container"
+            class="body-card"
+          >
+            <q-banner  v-for="(tarea, index) in tareas" :key="index" class="bg-primary text-white q-mb-sm">
+              <span class="text-h4"  > {{ tarea }}</span> 
+              <template v-slot:action>
+                <q-btn flat color="white" label="Eliminar" @click="eliminarTarea(index)"
+
+              /></template>
+            </q-banner>
+          </q-card-section>
+          <q-separator></q-separator>
+          <q-card-actions class="  row justify-end">
+            <q-btn
+              size="15px"
+              class="glossy"
+              round
+              color="deep-orange"
+              icon="add"
+              @click="prompt = true"
+            />
+          </q-card-actions>
+        </q-card>
+      </div>
+    </div>
+
+    <q-dialog v-model="prompt" persistent>
+      <q-card style="min-width: 350px">
+        <q-card-section>
+          <div class="text-h6">Nueva Tarea</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          <q-input
+            dense
+            autofocus
+            @keyup.enter="prompt = true"
+            id="tarea-descripcion"
+            v-model="tareaDescripcion"
+          />
+        </q-card-section>
+
+        <q-card-actions align="right" class="text-primary">
+          <q-btn flat label="Cancelar" v-close-popup />
+          <q-btn
+            id="btn-agregar"
+            flat
+            label="Agregar tarea"
+            v-close-popup = "status"
+            @click="addTask"
+          />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { LocalStorage } from "quasar"
+import { stringify } from 'querystring';
+
+export default {
+  data() {
+    return {
+      alert: false,
+      confirm: false,
+      prompt: false,
+      tareaDescripcion: '',
+      tareas: ['lavar','planchar'],
+      status : true
+    };
+  },
+  methods: {
+    addTask() {
+       this.status = true;
+      if (this.tareaDescripcion === '') {
+        alert("Debe suministrar un nombre de tarea valido");
+        this.status = false;
+      } else {
+        this.registrarTareas(this.tareaDescripcion);
+        this.tareaDescripcion = ''
+      }
+    },
+
+    registrarTareas(tarea: string) {
+      let index = this.tareas.length + 1
+      this.tareas.push(tarea)
+       window.localStorage.setItem(index.toString(),tarea)
+    },
+
+    mostraTareas()
+    {
+    
+       this.tareas = Object.values(localStorage)
+       console.log(this.tareas)
+     
+    },
+    eliminarTarea(item)
+    {
+      this.tareas.splice(item,1)
+      window.localStorage.removeItem(item)
+    }
+  },
+  created () {
+    this.mostraTareas()
+  }
+};
+</script>
+<style scoped>
+.card-header {
+  width: 70%;
+  min-height: 400px;
+}
+.body-card {
+  width: 100%;
+  min-height: 400px;
+}
+</style>
